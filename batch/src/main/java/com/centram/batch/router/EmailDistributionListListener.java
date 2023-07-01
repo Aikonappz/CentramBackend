b1b9e7cf38382a5d252c4024b4d50a193974c17a
sumit <babisumit@gmail.com>
2023-07-01 22:24:43 +0530

    imap connection updates

--- batch/src/main/java/com/centram/batch/router/EmailDistributionListListener.java
+++ batch/src/main/java/com/centram/batch/router/EmailDistributionListListener.java
@@ -1,24 +1,32 @@
 package com.centram.batch.router;
 
+import com.centram.core.service.ModuleService;
+import com.centram.domain.Module;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EmailDistributionListListener extends RouteBuilder {
     private static final Logger log = LoggerFactory.getLogger(EmailDistributionListListener.class);
 
-    private String interval = "0 0/3 * * * ?";
+    private final String interval = "0 0/3 * * * ?";
     @Value("${app.date.time.format:yyyy-MM-dd'T'HH:mm:ss}")
     private String dateTimeFormat;
     @Value("${app.report.path}")
     private String appReportPath;
+
+    @Autowired
+    private ModuleService moduleService;
 
     @Override
     public void configure() throws Exception {
@@ -31,9 +39,30 @@ public class EmailDistributionListListener extends RouteBuilder {
                     public void process(Exchange exchange) throws Exception {
                         Exchange ex = exchange;
                         String body = exchange.getIn().getBody(String.class);
-                        Map<String,Object> dataAttributes = new HashMap<String,Object>();
+                        Map<String, Object> dataAttributes = new HashMap<String, Object>();
+                        if (body.trim().length() > 0) {
+                            log.info("exchange body {}", body);
+                            String[] lines = body.split(System.getProperty("line.separator"));
+                            String[] line;
+                            String desc;
+                            for (int k = 0; k < lines.length; k++) {
+                                if (lines[k].contains("==>")) {
+                                    line = lines[k].split("==>");
+                                    dataAttributes.put(line[0].trim(), line[1].trim());
+                                } else continue;
+                            }
+                            List<String> keys = Arrays.asList("Category", "Sub Category", "Priority", "Title", "Description");
+                            for (String s : keys) {
+                                if (!dataAttributes.containsKey(s)) {
+                                    log.error("Key ==> {} not exist!",s);
+                                }
+                            }
 
-                        log.info("exchange body {}", body);
+                            //Module category = moduleService.getModuleByCustomerModuleName(String.valueOf(dataAttributes.get("Category")));
+                            //Module subCategory = moduleService.getModuleByCustomerModuleName(String.valueOf(dataAttributes.get("Sub Category")));
+                        }else{
+                            log.error("Body ==> {} not valid for incident creation!",body);
+                        }
                     }
                 })
                 .log(LoggingLevel.INFO, "junk-cleaner started -> ${header.CURRENT_DATE_TIME}")
